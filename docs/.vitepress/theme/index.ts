1bd0fa44cfae7c5273c1f228436bec1cc53b2804
yophon <1194944021@qq.com>
2026-05-24 13:25:03 +0800

    Add chapter navigation to docs pages

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,14 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
+import SeriesNav from "./SeriesNav.vue";
 import "./style.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => h(SeriesNav),
+      "doc-after": () => h(SeriesNav)
+    });
+  }
+};
